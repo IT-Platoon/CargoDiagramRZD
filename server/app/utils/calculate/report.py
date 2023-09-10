2bb7c02a0b0f48a2da2e9afcb319dfe06d33e42b
Nazar <furnoxl@yandex.ru>
2023-09-10 14:12:07 +0700

    Add words in docx

--- server/app/utils/calculate/report.py
+++ server/app/utils/calculate/report.py
@@ -10,7 +10,8 @@ async def create_document(
 ) -> None:
     doc = Document()
     doc.add_heading('Расчетно-пояснительная записка')
-    doc.add_paragraph(f"Продольное смещение грузов в вагоне: l(c) = {result['longitudinal_displacement_in_car']} мм. {result['longitudinal_displacement_flag']}")
+    paragraph = doc.add_paragraph(f"Продольное смещение грузов в вагоне: l(c) = {result['longitudinal_displacement_in_car']} мм. ")
+    paragraph.add_run(result['longitudinal_displacement_flag']).bold = True
     doc.add_paragraph(f"Продольное смещение грузов с вагоном: l(c) = {result['longitudinal_displacement_with_car']} мм")
     doc.add_paragraph(f"Поперечное смещение в вагоне: b = {result['lateral_displacement_in_car']} мм")
     doc.add_paragraph(f"Поперечное смещение с вагоном: b = {result['lateral_displacement_with_car']} мм")
